5c16de8cb967dce19db35055dfaae19b69de1020
KaitoMizukami <kaito.program@gmail.com>
2024-10-08 23:36:01 +0900

    Modify ip and port

--- src/main.go
+++ src/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 
-	"aws-intern/config"
 	"aws-intern/api"
+	"aws-intern/config"
 )
 
 func main() {
@@ -17,11 +19,16 @@ func main() {
 	r.GET("/", func(c *gin.Context) {
 		c.String(200, "AWS")
 	})
+
 	r.GET("/secret", func(c *gin.Context) {
 		c.String(200, "SUCCESS")
 	})
 
 	api.SetupV1Router(r, db)
 
-	r.Run(":8080")
+	ipAddress := "54.249.44.164"
+	port := "80"
+	if err := r.Run(ipAddress + ":" + port); err != nil {
+		log.Fatalf("Could not run server: %v", err)
+	}
 }
